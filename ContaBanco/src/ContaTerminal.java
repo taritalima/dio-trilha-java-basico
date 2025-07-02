e8203a02353a639306dea9c60f9605a129e9c43c
taritalima <tarita36@hotmail.com>
2025-07-02 17:00:21 -0300

    criação dos campos de entrada simples

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,31 @@
+
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+
+        int numeroConta;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        System.out.println("informe o seu nome: ");
+        nomeCliente = input.nextLine();
+
+        System.out.println("informe sua agencia: ");
+        agencia = input.nextLine();
+
+        System.out.println("informe o numero da conta: ");
+        numeroConta = input.nextInt();
+        input.nextLine();
+        
+        System.out.println("infome o saldo: ");
+        saldo = input.nextDouble();
+        input.nextLine();
+
+        System.out.printf("Olá %s, obrigada por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque", nomeCliente, agencia,numeroConta, saldo);
+
+        input.close();
     }
 }
